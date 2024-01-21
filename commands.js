42067c795ceb4744bd1f9ba21fe1c27b7e2b0175
rtoporoff <renan.toporoff@gmail.com>
2024-01-21 15:52:38 -0300

    continuação do projeto do site estilo terminal

---
+++ commands.js
@@ -0,0 +1,86 @@
+// var youtube = "https://www.youtube.com/fknight/";
+// var twitter = "https://www.twitter.com/forrestpknight/";
+// var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/rtoporoff/";
+var instagram = "https://www.instagram.com/nanoroff/";
+var github = "https://github.com/rtoporoff/";
+var email = 'mailto:renan.toporoff@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Forrest!👋",
+  "I'm a software developer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software", "development & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creator, and it's",
+  "the best decision I ever made.",
+  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
+  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
+  "because they're reading this right now, is that I also run a creative & media agency.",
+  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "Quem sou eu? Uma pergunta um tanto complexa...bom, eu sou o Renan Toporoff, mas todos me chamam de Nano, e estou migrando de carreira para ir atrás do meu sonho de ser um desenvolvedor.",
+  "<br>",
+  "Gosto de jogos eletrônicos, RPG, board games e doramas (pois é...)",
+  "<br>",
+  "Se quiser saber mais sobre mim, use o comando 'social' e me adicione para conversarmos! Foi um prazer e até logo!",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  // 'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
+  // 'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/rtoporoff' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/nanoroff' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/rtoporoff' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Mostrar minhas redes sociais',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       Ver meus projetos',
+  '<span class="command">history</span>        Ver o histórico de comandos',
+  '<span class="command">help</span>           Para ver a lista de comandos novamente',
+  '<span class="command">email</span>          Contate-me',
+  '<span class="command">clear</span>          Limpar terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Nano Dev</span>',
+  "  oooo   oooo                                          ooooooooo                           ",
+  "  8888o  88    ooooooo   oo oooooo     ooooooo         888    88o  ooooooooo8 oooo   oooo ",
+  "  88 888o88    ooooo888   888   888  888     888       888    888 888oooooo8   888   888  ",
+  "  88   8888  888    888   888   888  888     888       888    888 888           888 888   ",
+  "  o88o    88   88ooo88 8o o888o o888o  88ooo88        o888ooo88     88oooo888     888     ",
+                                                                                          
+  '<span class="color2">Bem vindo ao meu terminal interativo!</span>',
+  "<span class=\"color2\">Para a lista de comandos, digite:</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
